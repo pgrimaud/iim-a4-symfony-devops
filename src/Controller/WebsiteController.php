0fbda9365aec3a8b22e261c5f23a917b347a4e1f
Pierre Grimaud <grimaud.pierre@gmail.com>
2020-10-01 14:18:04 +0200

    Add fixtures & db with sqlite

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,14 @@ class WebsiteController extends AbstractController
     /**
      * @Route("/", name="website")
      */
-    public function index()
+    public function index(PostRepository $postRepository)
     {
+        $posts = $postRepository->findAll();
+
+        foreach($posts as $post){
+            echo $post->getTitle() . '<br/>';
+        }
+
         return $this->render('website/index.html.twig', [
             'controller_name' => 'WebsiteController',
         ]);
